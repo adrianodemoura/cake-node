7a2fc971748af5ad58fca2f6e73ca1d8554459b4
Adriano Moura <adrianodemoura@gmail.com>
2019-09-09 16:59:34 -0300

    inc luindo regra de duplicidade em papeis

--- modules/mac/orm/table/papeis.js
+++ modules/mac/orm/table/papeis.js
@@ -57,8 +57,43 @@ class Papeis extends Table {
      */
     async beforeValidate() {
         this.data['PapeAplicacaoId'] = configure('codigo_sistema')
+        gravaLog(this.data, 'dataPapel')
 
         return true
+    }
+
+    /**
+     * Método antes de salvar
+     * - verifica duplicidade.
+     * @param   {Object}    data    Dados do registro.
+     * @return  {Boolean}   boolean     Verdadeiro se deve continuar, Falso se não.
+     */
+    async beforeSave() {
+        const idUsuario     = this.data['PapeUsuarioId']
+        const idUnidade     = this.data['PapeUnidadeId']
+        const idPerfil      = this.data['PapePerfilId']
+        const idAplicacao   = this.data['PapeAplicacaoId']
+
+        if (! await this.verDupliciade()) {
+            return false
+        }
+
+        return true
+    }
+
+    async verDupliciade(idUsuario=0, idUnidade=0, idPerfil=0, idAplicacao=0) {
+
+        let params                  = {}
+        params.type                 = 'count'
+        params.where                = {}
+        params.where['usuario_id']  = idUsuario
+        params.where['unidade_id']  = idUnidade
+        params.where['perfil_id']   = idPerfil
+
+        const dataDup = await this.find(params)
+        gravaLog(dataDup, 'dataDup')
+
+        return false
     }
 }
 
